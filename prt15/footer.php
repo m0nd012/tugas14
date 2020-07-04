2c036a5ab8feb231f9f7a1595478996833b91354
m0nd012 <65072934+m0nd012@users.noreply.github.com>
2020-07-04 09:42:31 +0700

    Add files via upload

---
+++ prt15/footer.php
@@ -0,0 +1,14 @@
+<html>
+	<head>
+		<title>Contoh Modularisasi</title>
+		<link href="style.css" rel="stylesheet"
+type="text/stylesheet" media="screen" />
+	</head>
+	<body>
+		<div class="container">
+			<footer>
+				<p>Ari Purnomo 2020, TI STT Pelita Bangsa</p>
+			</footer>
+		</div>
+	</body>
+</html>
